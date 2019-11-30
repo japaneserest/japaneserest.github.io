e4064de7c5aaef65f799b77ab3a98b4ebd165ff3
japaneserest <57961773+japaneserest@users.noreply.github.com>
2019-11-30 18:03:41 +0300

    Add files via upload

--- build/js/script.js
+++ build/js/script.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});
+$(document).ready(function(){$('a[href*="#"]').on("click",function(t){t.preventDefault(),$("html, body").animate({scrollTop:$($(this).attr("href")).offset().top},700,"linear")})});
